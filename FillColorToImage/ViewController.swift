cee41899b23fe45866b141dcb7c0c050c082b73b
chenxuechen <chenxuechen159@126.com>
2017-01-20 11:11:49 +0800

    init project

---
+++ FillColorToImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FillColorToImage
+//
+//  Created by chenxuechen on 2017/1/20.
+//  Copyright © 2017年 chenxuechen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
